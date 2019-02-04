8751c8f5f11878461b6af09377211196eea4ae9b
erices <estudillo86@gmail.com>
2019-02-04 17:36:58 +0100

    e-11

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,14 +9,7 @@
     <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
-                    <div>
-                        <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
-                            <a href="{{$project->path()}}" class="text-black no-underline">{{$project->title}}</a>
-                        </h3>
-                        <div class="text-grey">{{str_limit($project->description,100)}}</div>
-                    </div>
-                </div>
+                @include('projects.partials.card')
             </div>
         @empty
             <div>No projects.</div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts.app')
 @section('content')
-    <h4>{{ $project->title }}</h4>
-    <p>{{ $project->description }}</p>
-    <a href="/projects">Go back</a>
+    <header class="flex items-center py-4">
+        <div class="flex justify-between items-center w-full">
+            <p class="text-grey text-sm font-normal">
+                <a href="/projects" class="text-sm font-normal text-grey no-underline">My Projects</a>
+                / {{$project->title}}
+            </p>
+            <a href="/projects/create" class="button ">New Project</a>
+        </div>
+    </header>
+    <main>
+        <div class="lg:flex -m-3">
+            <div class="lg:w-3/4 px-3 mb-8">
+                <div class="mb-6">
+                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                    {{--TASKS--}}
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                </div>
+                <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+                {{--General Notes--}}
+                <textarea class="card w-full" style="min-height:200px">Lorem ipsum.</textarea>
+            </div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                @include('projects.partials.card')
+            </div>
+        </div>
+
+    </main>
 @endsection
