e2d5f29033ed200ec74aea017e6e481712c6ddc2
erices <estudillo86@gmail.com>
2019-02-06 17:42:55 +0100

    e-15

--- app/Task.php
+++ app/Task.php
@@ -9,6 +9,8 @@ class Task extends Model
 {
   protected $fillable = ['body', 'project_id', 'completed'];
   
+  protected $touches=['project'];
+  
   public function path(): string
   {
     return \sprintf('/projects/%s/tasks/%s', $this->project->id, $this->id);
